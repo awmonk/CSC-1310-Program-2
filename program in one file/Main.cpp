3dc4fd3332ddeff95e61dac6f39f51e219e25253
Austin Monk <awmonk42@tntech.edu>
2023-10-02 06:34:46 -0500

    Housekeeping

--- program in one file/Main.cpp
+++ program in one file/Main.cpp
@@ -42,134 +42,24 @@ private:
         listNode *next;
         listNode *prev;
 
-        listNode(T value)
-            : value(value), next(nullptr), prev(nullptr) {}
-        ~listNode() { delete value; }
+        listNode(T value);
+        ~listNode();
     };
     listNode *head;
     listNode *tail;
-    void print(listNode *node)
-    {
-        cout << "\n";
-        if (node == nullptr)
-            return;
-        cout << *(node->value);
-        print(node->next);
-    };
-    typename List<T>::listNode *split(listNode *left, listNode *right)
-    {
-        if (left == nullptr || right == nullptr)
-            return nullptr;
 
-        listNode *inc = left;
-        listNode *dec = right;
-        listNode *prev = nullptr;
-
-        while (inc != nullptr && inc != dec)
-        {
-            prev = inc;
-            inc = inc->next;
-            if (inc == dec)
-                break;
-            dec = dec->prev;
-        }
-
-        if (prev)
-        {
-            prev->next = nullptr;
-            return inc;
-        }
-
-        return nullptr;
-    };
-    typename List<T>::listNode *merge(listNode *left, listNode *right)
-    {
-        if (left == nullptr)
-            return right;
-        if (right == nullptr)
-            return left;
-        if (left->value >= right->value)
-        {
-            left->next = merge(left->next, right);
-            left->next->prev = left;
-            left->prev = nullptr;
-            return left;
-        }
-        else
-        {
-            right->next = merge(left, right->next);
-            right->next->prev = right;
-            right->prev = nullptr;
-            return right;
-        }
-    };
-    typename List<T>::listNode *mergeSort(listNode *start, listNode *end)
-    {
-        if (start == nullptr || start->next == end)
-            return start;
-
-        listNode *left = nullptr;
-        listNode *right = nullptr;
-        listNode *mid = nullptr;
-
-        mid = split(start, end);
-
-        if (mid != nullptr)
-        {
-            left = mergeSort(start, mid->prev);
-            right = mergeSort(mid, end);
-        }
-
-        return merge(left, right);
-    };
+    void print(listNode *node);
+    typename List<T>::listNode *split(listNode *left, listNode *right);
+    typename List<T>::listNode *merge(listNode *left, listNode *right);
+    typename List<T>::listNode *mergesort(listNode *start, listNode *end);
 
 public:
-    List() : head(nullptr), tail(nullptr) {}
-    ~List()
-    {
-        listNode *node;
-        listNode *next;
-        node = tail;
-
-        while (node != nullptr)
-        {
-            next = node->prev;
-            delete node;
-            node = next;
-        }
-        cout << "\nGOODBYE!\n";
-        cout << endl;
-    };
-    void print()
-    {
-        print(head);
-    };
-    void append(T value)
-    {
-        listNode *node = new listNode(value);
-        if (head == nullptr)
-        {
-            head = node;
-            tail = node;
-        }
-        else
-        {
-            tail->next = node;
-            node->prev = tail;
-            tail = node;
-        }
-    };
-    void mergeSort()
-    {
-        head = mergeSort(head, tail);
-        listNode *current = head;
-        while (current != nullptr && current->next != nullptr)
-        {
-            current->next->prev = current;
-            current = current->next;
-        }
-        tail = current;
-    };
+    List();
+    ~List();
+    void print();
+    void append(T value);
+    void mergesort();
+    void outfile(const string &filename);
 };
 
 int main()
@@ -179,8 +69,8 @@ int main()
     string line, county, state;
     int index, pop;
 
-    // infile.open("counties_list.csv", ios::in);
-    infile.open("counties_ten.csv", ios::in);
+    infile.open("counties_list.csv", ios::in);
+    // infile.open("counties_ten.csv", ios::in);
 
     List<County *> list;
     County *newCounty;
@@ -211,9 +101,12 @@ int main()
     }
     infile.close();
 
-    // Call mergesort()
+    // Call mergesort
     cout << "\nCALLING MERGESORT\n";
-    list.mergeSort();
+    list.mergesort();
+
+    // Write to external file
+    list.outfile("sorted_counties.txt");
 
     // Print the list
     cout << "\nSORTED LIST\n";
@@ -222,6 +115,7 @@ int main()
     return 0;
 };
 
+/* County functions */
 County::County(int i, string n, string s, int p)
     : index(i), name(n), state(s), population(p){};
 int County::getIndex() { return index; };
@@ -229,5 +123,182 @@ int County::getPopulation() { return population; };
 void County::setIndex(int i) { index = i; };
 bool County::operator<(const County &c) { return population < c.population; };
 bool County::operator>(const County &c) { return population > c.population; };
+
+/* List functions */
+/* Private functions */
+// Node constructor
+template <class T>
+List<T>::listNode::listNode(T value)
+    : value(value), next(nullptr), prev(nullptr){};
+
+// Node destructor
+template <class T>
+List<T>::listNode::~listNode() { delete value; };
+
+// Private print
+template <class T>
+void List<T>::print(listNode *node)
+{
+    cout << "\n";
+    if (node == nullptr)
+        return;
+    cout << *(node->value);
+    print(node->next);
+};
+
+// mergesort split
+template <class T>
+typename List<T>::listNode *List<T>::split(listNode *left, listNode *right)
+{
+    if (left == nullptr || right == nullptr)
+        return nullptr;
+
+    listNode *inc = left;
+    listNode *dec = right;
+    listNode *prev = nullptr;
+
+    while (inc != nullptr && inc != dec)
+    {
+        prev = inc;
+        inc = inc->next;
+        if (inc == dec)
+            break;
+        dec = dec->prev;
+    }
+
+    if (prev)
+    {
+        prev->next = nullptr;
+        return inc;
+    }
+
+    return nullptr;
+};
+
+// mergesort merge
+template <class T>
+typename List<T>::listNode *List<T>::merge(listNode *left, listNode *right)
+{
+    if (left == nullptr)
+        return right;
+    if (right == nullptr)
+        return left;
+    if (left->value >= right->value)
+    {
+        left->next = merge(left->next, right);
+        left->next->prev = left;
+        left->prev = nullptr;
+        return left;
+    }
+    else
+    {
+        right->next = merge(left, right->next);
+        right->next->prev = right;
+        right->prev = nullptr;
+        return right;
+    }
+};
+
+// mergesort private
+template <class T>
+typename List<T>::listNode *List<T>::mergesort(listNode *start, listNode *end)
+{
+    if (start == nullptr || start->next == end)
+        return start;
+
+    listNode *left = nullptr;
+    listNode *right = nullptr;
+    listNode *mid = nullptr;
+
+    mid = split(start, end);
+
+    if (mid != nullptr)
+    {
+        left = mergesort(start, mid->prev);
+        right = mergesort(mid, end);
+    }
+
+    return merge(left, right);
+};
+
+/* Public functions */
+// List constructor
+template <class T>
+List<T>::List() : head(nullptr), tail(nullptr) {}
+
+// List destructor
+template <class T>
+List<T>::~List()
+{
+    listNode *node;
+    listNode *next;
+    node = tail;
+
+    while (node != nullptr)
+    {
+        next = node->prev;
+        delete node;
+        node = next;
+    }
+    cout << "\nGOODBYE!\n";
+    cout << endl;
+};
+
+// Public print (wrapper)
+template <class T>
+void List<T>::print() { print(head); };
+
+// List append
+template <class T>
+void List<T>::append(T value)
+{
+    listNode *node = new listNode(value);
+    if (head == nullptr)
+    {
+        head = node;
+        tail = node;
+    }
+    else
+    {
+        tail->next = node;
+        node->prev = tail;
+        tail = node;
+    }
+};
+
+// mergesort public (wrapper)
+template <class T>
+void List<T>::mergesort()
+{
+    listNode *current;
+    head = mergesort(head, tail);
+    current = head;
+    while (current != nullptr && current->next != nullptr)
+    {
+        current->next->prev = current;
+        current = current->next;
+    }
+    tail = current;
+};
+
+template <class T>
+void List<T>::outfile(const string &filename)
+{
+    ofstream outfile(filename);
+    if (!outfile.is_open())
+    {
+        cerr << "Error opening the file for writing." << endl;
+        return;
+    }
+
+    listNode *current = head;
+    while (current != nullptr)
+    {
+        outfile << *(current->value) << endl;
+        current = current->next;
+    }
+
+    outfile.close();
+}
 
 #endif
